8f1691cb5624e7ed65c2be62ef9efedc6e6a0012
Yao WANG <yao.wang@thoughtworks.com>
2023-03-23 21:57:10 +0800

    关闭几个接口

--- outlines/urls.py
+++ outlines/urls.py
@@ -5,10 +5,10 @@ from .api import *
 urlpatterns = [
     path('test', test),
     path('ready', get_one_ready_outline),
-    path('paragraphs', add_one_paragraph),
+    # path('paragraphs', add_one_paragraph),
     path('articles', add_articles),
-    path('paragraphs/finish', finish_paragraph),
-    path('paragraphs/title', add_title),
+    # path('paragraphs/finish', finish_paragraph),
+    # path('paragraphs/title', add_title),
     path('documents/down', download_document),
     path('documents/down/<str:file_name>', download_document_by_path),
     path('documents', documents),
